7c9325710da7dc61e6e1eb0af99e94345f81f27a
baozhida <baozhida88@126.com>
2017-04-13 11:26:56 +0800

    Update generate.sh

--- NodeChrome/generate.sh
+++ NodeChrome/generate.sh
@@ -5,5 +5,5 @@ echo "# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" > ./Dockerfile
 echo "# NOTE: DO *NOT* EDIT THIS FILE.  IT IS GENERATED." >> ./Dockerfile
 echo "# PLEASE UPDATE Dockerfile.txt INSTEAD OF THIS FILE" >> ./Dockerfile
 echo "# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" >> ./Dockerfile
-echo FROM selenium/node-base:$VERSION >> ./Dockerfile
+echo FROM baozhida/selenium-node-base:$VERSION >> ./Dockerfile
 cat ./Dockerfile.txt >> ./Dockerfile
