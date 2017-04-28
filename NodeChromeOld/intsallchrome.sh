39d9aa91de3a0dff1cd886f37c77c339a57ec724
baozhida <baozhida88@126.com>
2017-04-28 14:11:05 +0800

    Update intsallchrome.sh

--- NodeChromeOld/intsallchrome.sh
+++ NodeChromeOld/intsallchrome.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#CHROME_VERSION=48.0.2564.109
+#CHROME_VERSION=49.0.2623.75
+#CHROME_VERSION=50.0.2661.75
+#CHROME_VERSION=51.0.2704.84
+#CHROME_VERSION=53.0.2785.116
+#CHROME_VERSION=54.0.2840.71
+#CHROME_VERSION=55.0.2883.75
+#CHROME_VERSION=56.0.2924.87
 CHROME_VERSION=57.0.2987.133
 
 #============================================                                                                                                     
