f890f3ddfc9c06ca1c02b5a44ffeb62103151189
baozhida <baozhida88@126.com>
2017-04-28 11:30:15 +0800

    Update installchrome.sh

--- NodeBase2/installchrome.sh
+++ NodeBase2/installchrome.sh
@@ -1,7 +1,14 @@
-#/bin/sh
+#!/bin/bash
 
-sudo dpkg -i chrome64_57.0.2987.133.deb  >/dev/null  2>&1
+CHROME_VERSION=57.0.2987.133
+
+#============================================                                                                                                     
+# 下载 安装 Google Chrome                                                                                                                           
+#============================================                                                                                                     
+cd && sudo wget -q http://www.slimjetbrowser.com/chrome/lnx/chrome64_${CHROME_VERSION}.deb
+
+sudo dpkg -i chrome64_${CHROME_VERSION}.deb  >/dev/null  2>&1
 
 sudo apt-get -f install -y
 
-sudo dpkg -i chrome64_57.0.2987.133.deb
+sudo dpkg -i chrome64_${CHROME_VERSION}.deb
