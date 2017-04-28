414945769d7b22485bc75fa178dbf0cc8804425b
baozhida <baozhida88@126.com>
2017-04-28 13:12:29 +0800

    Update intsallchrome.sh

--- NodeChrome57Debug/intsallchrome.sh
+++ NodeChrome57Debug/intsallchrome.sh
@@ -5,7 +5,7 @@ CHROME_VERSION=57.0.2987.133
 #============================================                                                                                                     
 # 下载 安装 Google Chrome                                                                                                                           
 #============================================                                                                                                     
-cd && sudo wget -q http://www.slimjetbrowser.com/chrome/lnx/chrome64_${CHROME_VERSION}.deb
+sudo wget -q http://www.slimjetbrowser.com/chrome/lnx/chrome64_${CHROME_VERSION}.deb
 
 sudo dpkg -i chrome64_${CHROME_VERSION}.deb  >/dev/null  2>&1
 
